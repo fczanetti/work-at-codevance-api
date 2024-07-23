137e6dfd517c00dace2f98393afbd260dc9f47b1
Fabio Zanetti <fczanetti@hotmail.com>
2024-07-23 17:08:57 -0300

    Validate payment due_date
    Close #23

--- codevance_api/payments/serializers.py
+++ codevance_api/payments/serializers.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from rest_framework import serializers
 
 from codevance_api.payments.models import Payment, Supplier
@@ -16,4 +18,13 @@ class PaymentSerializer(serializers.ModelSerializer):
         """
         if not value > 0:
             raise serializers.ValidationError("Make sure the value is greater than zero.")
+        return value
+
+    def validate_due_date(self, value):
+        """
+        Certifies the due_date informed is not
+        before the day of payment creation.
+        """
+        if not value >= date.today():
+            raise serializers.ValidationError("Inform a date greater than or equal today.")
         return value
